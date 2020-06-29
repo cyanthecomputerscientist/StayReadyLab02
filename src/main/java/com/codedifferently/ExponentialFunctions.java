fe2bc2610454fb249a9501afebbfe1186b4f83f6
Dominique <cyanthecomputerscientist@gmail.com>
2020-06-29 04:03:53 -0400

    No functionality implemented fails unit test

--- src/main/java/com/codedifferently/ExponentialFunctions.java
+++ src/main/java/com/codedifferently/ExponentialFunctions.java
@@ -1,6 +1,38 @@
 package com.codedifferently;
 
 public class ExponentialFunctions {
-    double value;    
+    private double value;    
+    
+    double getValue()
+    {
+        return 0.0;
+    }
+    void setValue(double val)
+    {
 
+    }
+    double powerOf(double val, double power)
+    {
+        return 0.0;
+    }
+    double squareRootOF(double val)
+    {
+        return 0.0;
+    }
+    double cubedRootOf(double val)
+    {
+        return 0.0;
+    }
+    double commonLogOf(double val)
+    {
+        return 0.0;
+    }
+    double naturalLogOf(double val)
+    {
+        return 0.0;
+    }
+    double expFunction(double power)
+    {
+        return 0.0;
+    }
 }
